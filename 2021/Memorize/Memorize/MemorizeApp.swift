e9a933a6a71e5c58559bc0bdb4196bfd3b0bf47f
Kim, Min Ho <xho1995@gmail.com>
2022-03-23 19:50:40 +0900

    add a new project called Memorize

---
+++ 2021/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Min Ho Kim on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
